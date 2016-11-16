8ddeed53c794826fbec71be1d48232e4e517f0c8
Zheng Gao <zgao@axioma.com>
2016-11-16 18:05:21 -0500

    Finished latex document

--- python/plot2d.py
+++ python/plot2d.py
@@ -4,23 +4,30 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 
-CBprice = np.loadtxt('D:/ConvertibleBond/cs/ConvertibleBond/ConvertibleBond/bin/Debug/CBsol.txt')
-#CBprice = np.loadtxt('D:/COCBsol.txt')
-CBprice = np.matrix.transpose(CBprice)
-
+path = "D:/"
+#fnames = ["CBsol_zero", "CBsol_coupon", "CBsol_put", "CBsol_callput"]
+#decros = ["-", ".", ".-", "--"];
+#legends = ["Zero-coupon", "Coupon payment", "Coupon plus put", "Coupon plus put and call"];
+fnames = ["CBsol"]
+decros = ["-"]
+legends = ["CB"]
 fig = plt.figure()
-msize = CBprice.shape
-B = 101
-Smax = 500
-Nx = msize[0]
-Nt = msize[1]
-S = np.linspace(0, Smax, Nx)
 
+for i in range(0, len(fnames)):
+    V = np.loadtxt(path + fnames[i] + ".txt")
+    V = np.matrix.transpose(V)
+    msize = V.shape
+    B = 100
+    Smax = 500
+    Nx = msize[0]
+    Nt = msize[1]
+    S = np.linspace(0, Smax, Nx)
+    plt.plot(S, V[:, -1], decros[i])
 
-for k in range(1, Nt, 50):
-    plt.plot(S, CBprice[:, k]);
-
-axes = plt.gca()
-axes.set_xlim([20, 160])
-axes.set_ylim([100, 160])
+plt.legend(legends, loc = "upper left")
+plt.xlabel("Stock Price")
+plt.ylabel("Convertible Bond Value")
+#axes = plt.gca()
+#axes.set_xlim([20, 160])
+#axes.set_ylim([50, 160])
 plt.show()
--- python/plot3d.py
+++ python/plot3d.py
@@ -3,24 +3,34 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
+from pylab import *
 
-CBprice = np.loadtxt('D:/ConvertibleBond/cs/ConvertibleBond/ConvertibleBond/bin/Debug/COCBsol.txt')
-#CBprice = np.loadtxt('D:/CBsol.txt')
-CBprice = np.matrix.transpose(CBprice)
+#CBprice = np.loadtxt('D:/ConvertibleBond/cs/ConvertibleBond/ConvertibleBond/bin/Debug/CBsol.txt')
+V = np.loadtxt('D:/CBsol.txt')
+V = np.matrix.transpose(V)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-msize = CBprice.shape
+msize = V.shape
 T = 5
-B = 101
-Smax = 160
+B = 100
+Smax = 5*B
 Nt = msize[1]
 Nx = msize[0]
 t = np.linspace(T, 0, Nt)
 S = np.linspace(0, Smax, Nx)
 X, Y = np.meshgrid(t, S)
-surf = ax.plot_surface(X, Y, CBprice, rstride=1, cstride=1, cmap=cm.coolwarm,
-                   linewidth=0, antialiased=False)
-
+Vmin = 90
+Vmax = 160
+V[V < Vmin] = NaN
+V[V > Vmax] = NaN
+#V[Y > 160] = NaN;
+ax.set_zlim(Vmin, Vmax)
+ax.set_ylim(20, 160)
+surf = ax.plot_surface(X, Y, V, rstride=1, cstride=1, cmap=cm.coolwarm,
+                   linewidth=0, antialiased=False, vmin = Vmin, vmax = Vmax)
+ax.set_xlabel("Time [years]")
+ax.set_ylabel("Stock price [$]")
+ax.set_zlabel("Convertible Bond Price [$]")
 fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
