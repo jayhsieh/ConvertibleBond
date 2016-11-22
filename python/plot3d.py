17da869227f442519112d069ebf515dd627f8a49
Zheng Gao <zgao@axioma.com>
2016-11-22 11:45:57 -0500

    Update python script and latex figures

--- python/plot3d.py
+++ python/plot3d.py
@@ -5,32 +5,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pylab import *
 
-#CBprice = np.loadtxt('D:/ConvertibleBond/cs/ConvertibleBond/ConvertibleBond/bin/Debug/CBsol.txt')
-V = np.loadtxt('D:/CBsol.txt')
-V = np.matrix.transpose(V)
+V = np.loadtxt('D:/Results/CBsol.txt')
+t = np.loadtxt('D:/Results/time.txt')
+
+#parameters for CB
+Vmin = 95; Vmax = 165
+Ymin = 20; Ymax = 160
+Smax = V[0,-1]
+angle = -30
+meshstride = 8
+
+#parameters for COCB
+#Vmin = 0; Vmax = amax(V);
+#Ymin = 0; Ymax = 200
+#Smax = 500
+#angle = 20
+#meshstride = 20
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 msize = V.shape
-T = 5
-B = 100
-Smax = 5*B
-Nt = msize[1]
-Nx = msize[0]
-t = np.linspace(T, 0, Nt)
+T = t[0]
+print(T, msize, len(t))
+
+Nt = msize[0]
+Nx = msize[1]
 S = np.linspace(0, Smax, Nx)
-X, Y = np.meshgrid(t, S)
-Vmin = 90
-Vmax = 160
+X, Y = np.meshgrid(S, t)
+
 V[V < Vmin] = NaN
 V[V > Vmax] = NaN
-#V[Y > 160] = NaN;
+V[X < Ymin] = NaN
+V[X > Ymax] = NaN
 ax.set_zlim(Vmin, Vmax)
-ax.set_ylim(20, 160)
-surf = ax.plot_surface(X, Y, V, rstride=1, cstride=1, cmap=cm.coolwarm,
+ax.set_ylim(Ymin, Ymax)
+
+surf = ax.plot_surface(Y, X, V, cmap=cm.coolwarm, cstride=1, rstride=1,
                    linewidth=0, antialiased=False, vmin = Vmin, vmax = Vmax)
+#ax.plot_wireframe(Y, X, V, rstride=meshstride, cstride=meshstride)
 ax.set_xlabel("Time [years]")
 ax.set_ylabel("Stock price [$]")
-ax.set_zlabel("Convertible Bond Price [$]")
+ax.set_zlabel("CB [$]")
 fig.colorbar(surf, shrink=0.5, aspect=5)
+ax.view_init(30, angle)
 plt.show()
